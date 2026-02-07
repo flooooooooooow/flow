82e53cb8a5080b7c0ac543356b51fdc99e58ad14
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-02-07 14:40:15 +0000

    Expand Vulkan ABI with frame and buffer ops

--- demos/vulkan_abi/renderer.h
+++ demos/vulkan_abi/renderer.h
@@ -16,6 +16,13 @@ void flow_vk_2048_poll(void);
 int32_t flow_vk_2048_key_down(int32_t key);
 void flow_vk_2048_draw(const float* instance_data, int32_t count);
 void flow_vk_2048_upload_texture(const uint8_t* pixels, int32_t width, int32_t height);
+int32_t flow_vk_begin_frame(void);
+void flow_vk_end_frame(void);
+int32_t flow_vk_create_instance_buffer(int32_t capacity);
+void flow_vk_update_instance_buffer(int32_t handle, const float* instance_data, int32_t count);
+void flow_vk_draw_instance_buffer(int32_t handle, int32_t count);
+int32_t flow_vk_create_texture(int32_t width, int32_t height);
+void flow_vk_update_texture(int32_t handle, const uint8_t* pixels, int32_t width, int32_t height);
 
 #ifdef __cplusplus
 }
