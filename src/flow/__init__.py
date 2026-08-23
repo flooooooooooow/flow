06ba3eb429e2bb0db5f0aa1760bf0cbb68dba776
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-23 05:39:50 +0100

    fix(dsys): install expander correctness guards

--- src/flow/__init__.py
+++ src/flow/__init__.py
@@ -3,4 +3,9 @@ FLOW Language Compiler Package
 """
 
 from .version import __version__ as __version__
+from ._dynamics_dsl_fixes import install as _install_dynamics_dsl_fixes
+
+_install_dynamics_dsl_fixes()
+del _install_dynamics_dsl_fixes
+
 from .monomorphize import monomorphize as monomorphize, Monomorphizer as Monomorphizer
